cc007a444844304d297757787f7d7035a0531b4a
White, Clarrissa <ciwhite@gannett.com>
2015-01-09 20:40:35 -0600

    added gherkin to .feature and wrote step definition code for Order Dominos Pizza Online

--- features/cwtraining.feature
+++ features/cwtraining.feature
@@ -1,11 +1,22 @@
+@orderonline
 Feature: Order Dominos Pizza Online
 We want to step through a simple scenario for training
 
+@homepage
 Scenario: Check the Dominos home page
   When I visit the Dominos home page
   Then I should see the Dominos home page
 
-Scenario: Location Search for Dominos Pizza
+@address
+Scenario: Complete address, select store and
   Given I visit the Dominos home page
-  When I click the "Order Online" tab
+  When I click the Order Online tab
   Then I should see the Dominos Location Search page
+  When I click the Delivery button
+    And I select House from the Address Type select box
+    And I enter 2554 W 119TH ST in the Street Address field
+    And I enter CHICAGO in the City field
+    And I select IL from the State select box
+    And I enter 60655 in the Zip Code field
+    And I click the Continue button
+  Then I should see the Entrees page
--- features/step_definitions/cwtraining_steps.rb
+++ features/step_definitions/cwtraining_steps.rb
@@ -1,3 +1,4 @@
+# home page
 When(/^I visit the Dominos home page$/) do
     visit('https://order.dominos.com/')
 end
@@ -6,10 +7,43 @@ Then(/^I should see the Dominos home page$/) do
     expect(page).to have_selector(:id, 'homePage')
 end
 
-When(/^I click the "(.*?)" tab$/) do |tab|
-    click_link('Order Online')
+# start order online process and complete address form
+When(/^I click the Order Online tab$/) do
+  click_link('Order Online')
 end
 
 Then(/^I should see the Dominos Location Search page$/) do
-    expect(page).to have_selector(:id, 'locationsSearchPage')
+  expect(page).to have_selector(:id, 'locationsSearchPage')
+end
+
+When(/^I click the Delivery button$/) do
+  find('.Delivery').click
+end
+
+When(/^I select House from the Address Type select box$/) do
+  select('House',:from => 'Address_Type_Select')
+end
+
+When(/^I enter 2554 W 119TH ST in the Street Address field$/) do
+  fill_in('Street', :with => '2554 W 119TH ST')
+end
+
+When(/^I enter CHICAGO in the City field$/) do
+  fill_in('City', :with => 'CHICAGO')
+end
+
+When(/^I select IL from the State select box$/) do
+  select('IL', :from => 'Region')
+end
+
+When(/^I enter 60655 in the Zip Code field$/) do
+  fill_in('Postal_Code', :with => '60655')
+end
+
+When(/^I click the Continue button$/) do
+  click_button('Continue')
+end
+
+Then(/^I should see the Entrees page$/) do
+  expect(page).to have_selector(:id, 'entreesPage')
 end
